d755d91c382261da9e42a208f3deec3274d65f88
RushATom <73178729+RushATom@users.noreply.github.com>
2020-10-21 09:25:26 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,28 @@
+const Disocrd - require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready',function() {
+	bot.user.setGame("Command: :help);
+	console.log("Connected");
+});
+
+bot.login("NzY3ODYxMzc0ODk3ODgxMTM4.X44EoQ.Fznik41xeCb2dpARgd4KNQuQhf8");
+
+
+bot.on('message', message => {
+	if (message.content === prefix + "help"){
+		message.chanel.sendMessage("Liste des commandes: \n !tt \n !fabriquant");
+	}
+	
+	if (message.content === prefix + "tt"){
+		message.channel("Création du bot le _21/10/2020_ à _9h35_");
+		console.log("Commande effectué");
+	}
+	
+	if (message.content === prefix + "fabriquant"){
+		message.reply("_Ce bot à été crée par RushATom_");
+		console.log("Commande effectué");
+	}
+});		
