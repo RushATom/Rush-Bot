c9b5491bebc53d30b6d9e58150b60c74ba9d551b
RushATom <73178729+RushATom@users.noreply.github.com>
2020-10-21 09:45:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready',function() {
 	console.log("Connected");
 });
 
-bot.login("NzY3ODYxMzc0ODk3ODgxMTM4.X44EoQ.IxP5wlO2EaZmCBUmtyisALkr42A");
+bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
